28cb1e809d828e0f2e95a26f63fd65ea58886cf8
NiniKv <nini.kv16@gmail.com>
2024-08-09 00:46:15 +0400

    davaleba java script

---
+++ davaleba.js
@@ -0,0 +1,14 @@
+// დაწერეთ ფუნქცია რომელსაც გადასცემ ორ რიცხვს (m,n) და დააბრუნებს რომელიც მეტია იმ რიცხვს , თუ რიცხვები ტოლია დააბრუნებს 0 -ს
+function numbers(m,n){
+    if (m>n){
+        console.log (m)
+} else if (n>m){
+    return n
+} else return 0
+}
+
+// დაწერეთ ფუნქცია რომელსაც გადასცემთ ორ ცვლადს(რიცხვს) , დააბრუნეთ რიცხვების ჯამი
+function totalNumber(a,b){
+    return a+b
+}
+console.log(totalNumber(10,20))
